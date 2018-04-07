30247614c38cf60d30ed6f15d1d361e6f85cf3be
Sanchai Wora-apinyaporn <sanchai-wp@hotmail.com>
2018-04-07 22:06:51 +0700

    challenges 3.1

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.getElementById('dice-1').src = 'dice-' + dice2 + '.png';
 
         // 3. Update the round score If the rolled number was NOT a 1
-        if (dice1 !== 1 && dice2 !== 1) {
+        if (dice1 !== 1 || dice2 !== 1) {
             //Add Score
             roundScore += dice1 + dice2;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
